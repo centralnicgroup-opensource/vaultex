e6545b5d8485ac8e595a5919ea80dffd5de2436a
Benny C. Wong <bwong337@gmail.com>
2016-04-13 14:50:56 -0700

    increment version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Vaultex.Mixfile do
 
   def project do
     [app: :vaultex,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
